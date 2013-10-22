9390c9aab3a0e5fcc585b2fc72e6b494cf22b6ef
tan <tanmaykm@gmail.com>
2013-10-22 17:56:45 +0530

    split network and raster modes

---
+++ src/cs_state.py
@@ -0,0 +1,21 @@
+
+class CSState:
+    def __init__(self):
+        self.amg_hierarchy = None
+        self.cellsize = None
+        self.g_map = None
+        self.ground_map = None
+        self.includedPairs = None
+        self.lastUpdateTime = None
+        self.mask = None
+        self.ncols = None
+        self.nodata = None
+        self.nrows = None
+        self.pointStrengths = None
+        self.points_rc = None
+        self.poly_map = None
+        self.source_map = None
+        self.startTime = None
+        self.version = None
+        self.xllcorner = None
+        self.yllcorner = None
